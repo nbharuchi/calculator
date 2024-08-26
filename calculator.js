27a634932f6047754d9ab222e68e32bb89535258
@nbharuchi <97713275+nbharuchi@users.noreply.github.com>
2024-08-26 12:28:43 -0400

    create new function called operate that takes in operator and two numbers and call that function based on the operator

--- calculator.js
+++ calculator.js
@@ -18,3 +18,25 @@ let num1;
 let operator;
 let num2;
 
+function operate(operator, num1, num2) {
+    let result;
+    if (operator === "plus" || operator === "+") {
+        result = add(num1, num2);
+    } else if (operator === "minus" || operator === "-") {
+        result = subtract(num1, num2)
+    } else if (operator === "multiply" || operator === "*") {
+        result = multiply(num1, num2);
+    } else if (operator === "divide" || operator === "/") {
+        if (num2 / 0) {
+            console.error("Oops! division by zero!");
+            return undefined;
+        }
+        result = divide(num1, num2);
+    }
+    else {
+        console.error("unknown operator");
+        return undefined;
+    }
+    console.log("result: ", result);
+    return result;
+}
